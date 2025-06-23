ae77f33a45e1df6f2118e27cc9e82d0499544f5c
Jasmin Salvador <sjasmin.salvador@gmail.com>
2025-06-22 21:02:14 -0400

    se agrega modal con test a página de consejos y lógica js

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+//modal test seguridad / asignar puntaje/ mostrar mensaje
+$(document).ready(function () {
+    $("#testSeguridad").on("submit", function (e) {
+        e.preventDefault();
+
+        const resp1 = $('input[name="preg1"]:checked').val();
+        const resp2 = $('input[name="preg2"]:checked').val();
+        const resp3 = $('input[name="preg3"]:checked').val();
+
+        let puntaje = 0;
+
+        if (resp1 === "falso") puntaje++;
+        if (resp2 === "falso") puntaje++;
+        if (resp3 === "verdadero") puntaje++;
+
+        let mensaje = '';
+
+        if (puntaje === 3) {
+            mensaje = "3 puntos. ¡Felicidades 🎉! Sabes mucho sobre seguridad.";
+        } else if (puntaje === 2) {
+            mensaje = "2 puntos. ¡Buen trabajo 👋! Aunque puedes mejorar algunos aspectos.";
+        } else if (puntaje === 1) {
+            mensaje = "1 punto. Sería bueno repasar los consejos sobre seguridad 💡"
+        } else {
+            mensaje = "0 puntos. ¡Definitivamente necesitas aprender sobre seguridad! 🚨"
+        }
+
+        // mostrar resaultado
+        $("#resultadoTest").text(mensaje).removeClass("d-none");
+        $("#btnVolverIntentar").removeClass("d-none");
+    });
+
+    // btn volver a intentar
+    $("#btnVolverIntentar").on("click", function () {
+        // reset test
+        $("#testSeguridad")[0].reset();
+
+        // ocultar resultado
+        $("#resultadoTest").addClass("d-none");
+
+        // volver a ocultar btn volver a intentar
+        $("#btnVolverIntentar").addClass("d-none");
+    });
+
+    // cerrar modal
+    $("#cerrarModal").on("click", function () {
+        // limpiar test
+        $("#testSeguridad")[0].reset();
+
+        // volver a ocultar btn volver a intentar
+        $("#btnVolverIntentar").addClass("d-none");
+    });
+});
